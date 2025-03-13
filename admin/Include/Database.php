99de2d6e88821b5f79df00e5435c1f4169c00329
mirfani340 <muhammadirfani@pm.me>
2025-03-13 20:26:06 +0700

    adding db port

--- admin/Include/Database.php
+++ admin/Include/Database.php
@@ -1,12 +1,13 @@
 <?php
-$server = '206.189.81.94';
-$username = 'ponpes';
-$password = 'ponpes340';
+$server = '51.79.192.199';
+$username = 'ponpes_admin';
+$password = 'ustt7s47YzHyFT';
 $db_name = 'ponpes';
+$port = 6033; // Define your MySQL port here
 $con;
 
 try{
-	$con = mysqli_connect($server, $username, $password, $db_name) or die(mysqli_connect_errno());
+	$con = mysqli_connect($server, $username, $password, $db_name, $port) or die(mysqli_connect_errno());
 	
 }catch(Exception $e){
 	echo $e->getMessage();
